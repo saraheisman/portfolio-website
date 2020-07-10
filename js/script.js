a29984d7059d19600fe5d8723aa5f800daffae1d
Sarah <sarah.m.eisman@gmail.com>
2020-07-10 17:29:07 -0400

    Add JS responsive menu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 console.log("Hello, I'm Sarah! Welcome to my design portfolio.");
+
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+        x.className += ' responsive';
+    } else {
+        x.className = 'navtoggle';
+    }
+}
